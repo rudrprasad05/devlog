0ebe20aa03dff2ee3926f58133a2c97a022ba898
Rudr <rudrprasad@yahoo.com>
2024-01-05 17:18:35 +1200

    fixes2

--- types/index.ts
+++ types/index.ts
@@ -1,14 +1,3 @@
-import { User, Tag, Product, Order, OrderDetails } from "@prisma/client";
+import { User } from "@prisma/client";
 
 export type UserType = User;
-
-export type ProductType = Product & {
-  tag: Tag;
-};
-
-export type OrderType = Order & {
-  customer: User;
-  orderDetails: OrderDetails;
-};
-
-export type TagType = Tag;
